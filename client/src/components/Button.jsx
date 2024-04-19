e24d5cc99d2de3d9ae111cf16334c1ffebee9383
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2024-04-19 10:57:08 +0300

    ,

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({text}) => {
   return (
     <button
       type='button'
-      className='text-white bg-primary-500 hover:bg-white hover:text-primary-500 hover:border hover:border-primary-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center '>
+      className='text-white bg-primary-500 hover:bg-white hover:text-primary-500 hover:border hover:border-primary-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4  md:py-2 text-center '>
       {text}
     </button>
   );
