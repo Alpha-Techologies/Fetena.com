cbb0676cbc7758c5aaa24b01f2d7155a76f1e1a6
Yonas Enchalew <yonasenchalew@gmail.com>
2024-04-26 15:29:24 +0300

    fileUpload

--- server/app.js
+++ server/app.js
@@ -22,6 +22,7 @@ const limiter = rateLimit({
 });
 const methodOverride = require("method-override");
 
+const fileUpload =  require("express-fileupload");
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "http://localhost:4000");
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -46,6 +47,10 @@ app.use(
 );
 
 app.use(methodOverride("_method"));
+
+
+
+app.use(fileUpload());
 
 //Middlewares
 app.use(
