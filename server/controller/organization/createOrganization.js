08bb5588341d307e779fe8e934941860c1adaf81
Yohannes Mesganaw <yohannesmesg@gmail.com>
2024-05-15 22:46:59 +0300

    updated create organization and fixed josey merge issue

--- server/controller/organization/createOrganization.js
+++ server/controller/organization/createOrganization.js
@@ -3,9 +3,10 @@ const Organization = require("../../models/organization.model");
 const User = require("../../models/user.model");
 const catchAsync = require("../../utils/catchAsync");
 const factory = require("../handlerFactory");
+const APIError = require("../../utils/apiError");
+const { fileUpload } = require("../profile/fileUpload");
 
 exports.createOrganization = catchAsync(async (req, res, next) => {
-
   if (!req.files) {
     return next(new APIError("There is no file", StatusCodes.BAD_REQUEST));
   }
@@ -16,8 +17,8 @@ exports.createOrganization = catchAsync(async (req, res, next) => {
 
   const orgLogo = req.files.logo;
   const parsedBody = JSON.parse(req.body.data);
-
   const userId = req.user.id;
+  parsedBody.adminUser = userId;
 
   if (!orgLogo.mimetype.startsWith("image")) {
     return next(
@@ -25,17 +26,24 @@ exports.createOrganization = catchAsync(async (req, res, next) => {
     );
   }
 
-
-  const doc = await Organization.create(newOrganization);
+  const newOrganization = await Organization(parsedBody);
 
   const user = await User.findOne({ _id: userId });
 
-  user.addAsAdmin(doc._id);
+  user.addAsAdmin(newOrganization._id);
 
+  newOrganization.logo = await fileUpload({
+    file: orgLogo,
+    name: `organizationLogo_` + newOrganization._id,
+    filePath: "organizationsLogo",
+    maxSize: 1024 * 1024,
+  });
+
+  await newOrganization.save();
   // await user.save();
 
   res.status(StatusCodes.CREATED).json({
     sucess: true,
-    data: doc,
+    data: newOrganization,
   });
 });
