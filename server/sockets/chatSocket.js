2a991f159f7fcdddb6eaac87fce552f177a45f93
Yohannes Teshome <83913756+johannesteshome@users.noreply.github.com>
2024-06-04 13:11:37 +0300

    .

--- server/sockets/chatSocket.js
+++ server/sockets/chatSocket.js
@@ -1,20 +1,19 @@
-
 // initialize the chat socket
-const users = {};  // This maps userId to socketId
+const users = {}; // This maps userId to socketId
 
 const chatSocket = (io, socket) => {
   // Join a room
-  socket.on('joinRoom', ({ userId, roomId }) => {
+  socket.on("joinRoom", ({ userId, roomId }) => {
     socket.join(roomId);
     users[userId] = { socketId: socket.id, roomId };
     console.log(`User ${userId} joined room ${roomId}`);
   });
 
   // Handle sending a chat message
-  socket.on('sendMessage', ({ userId, roomId, message }) => {
+  socket.on("sendMessage", ({ userId, roomId, message }) => {
     if (users[userId] && users[userId].roomId === roomId) {
       console.log(`User ${userId} sent message to room ${roomId}: ${message}`);
-      io.in(roomId).emit('chatMessage', { userId, message });
+      io.in(roomId).emit("chatMessage", { userId, message });
     } else {
       console.log(`User ${userId} not in room ${roomId}`);
     }
