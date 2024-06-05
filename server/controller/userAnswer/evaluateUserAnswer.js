d6b2c01a6315ea8939ba3ae62a99eae4e611e3be
Tibebe Solomon <87706650+Tibex88@users.noreply.github.com>
2024-06-05 10:18:14 +0300

    eval basic

--- server/controller/userAnswer/evaluateUserAnswer.js
+++ server/controller/userAnswer/evaluateUserAnswer.js
@@ -31,61 +31,12 @@ exports.evaluateUserAnswer = catchAsync(async (req, res, next) => {
   const apiKey = process.env.TOGETHER_API_KEY;
   const togetherManager = new TogetherManager(apiKey, _, true);
 
-  // userAnswer.questionAnswers.forEach(async (userAnswer) => {
-  //   // console.log('userAnswer',userAnswer,'\n')
-  //   let question = null;
-  //   for (let i = 0; i < questions.length; i++) {
-  //       if (questions[i]._id.equals(userAnswer.questionId._id)) {
-  //           question = questions[i];
-  //           break;
-  //       }
-  //   }
-  //   if (question) {
-  //     // Handle different question types separately 
-  //     const userAnswerText = userAnswer.answerText.toLowerCase().trim();
-  //     const correctAnswerText = question.correctAnswer.toLowerCase().trim();
-  //     // console.log(userAnswerText,correctAnswerText,'\n')
-
-  //       if (question.questionType === 'True/False' || question.questionType === 'choose' || question.questionType === 'matching') {
-  //           // Perform case-insensitive string comparison for true/false and choice questions
-  //           // here
-  //           // if (userAnswerText === correctAnswerText) {
-  //           //     userAnswer.point = question.points 
-  //           //     totalScore += question.points; // Increment score if answers match
-  //           // }
-  //       } else {
-  //           // Handle other question types (e.g., short userAnswer.questionAnswers[i], essay) here
-  //           // Add your logic for evaluating those types of questions
-  //           // For simplicity, we'll just assume all other types are manually graded
-  //           // and give full points if the userAnswer.questionAnswers[i] is not empty
-  //           console.log('questionType',question.questionType,'\n')
-  //           if (userAnswer.answerText.trim() !== '') {
-  //             const prompt =
-  //             "I am giving you an a question in a json file try to understand and and create an appropriate tags for this question and in your return make it a json file that has an object with a key tag and an array of string that are going to be the tags and make the tags short and concise and userAnswer.questionAnswers[i] me with a json format string in which i can take your output and use the json.parse" +
-  //             JSON.stringify({
-  //               question: question.questionText,
-  //               options: question.questionChoice,
-  //               type: question.questionType,
-  //             });
-  //             const result = await togetherManager.performInference(prompt);
-  //             console.log('result',result)
-  //             userAnswer.point = question.points
-  //             totalScore += 0; // Give full points if userAnswer.questionAnswers[i] is not empty
-  //               // totalScore += question.points; // Give full points if userAnswer.questionAnswers[i] is not empty
-  //           }
-  //       }
-  //   }
-  // });
-  // let userAnswer.questionAnswers[i] = null
   for (let i = 0; i < userAnswer.questionAnswers.length; i++) {
 
-    // userAnswer.questionAnswers[i] = userAnswer.questionAnswers[i]
-  // console.log('userAnswer',userAnswer,'\n')
     let question = null;
     for (let j = 0; j < questions.length; j++) {
         if (questions[j]._id.equals(userAnswer.questionAnswers[i].questionId._id)) {
             question = questions[j];
-            // console.log(question)
             break;
         }
     }
@@ -93,46 +44,33 @@ exports.evaluateUserAnswer = catchAsync(async (req, res, next) => {
       // Handle different question types separately 
       const answerText = userAnswer.questionAnswers[i].answerText.toLowerCase().trim();
       const correctAnswerText = question.correctAnswer.toLowerCase().trim();
-      // console.log(answerText,correctAnswerText,'\n')
 
         if (question.questionType === 'True/False' || question.questionType === 'choose' || question.questionType === 'matching') {
             // Perform case-insensitive string comparison for true/false and choice questions
             // here
-            // if (answerText === correctAnswerText) {
-            //     userAnswer.questionAnswers[i].point = question.points 
-            //     totalScore += question.points; // Increment score if answers match
-            // }
+            if (answerText === correctAnswerText) {
+                userAnswer.questionAnswers[i].point = question.points 
+                totalScore += question.points; // Increment score if answers match
+            }
         } else {
             // Handle other question types (e.g., short answer, essay)
             if (userAnswer.questionAnswers[i].answerText.trim() !== '') {
-              const prompt = shortAnswer(question.questionText, question.correctAnswer, question.points, question.correctAnswer, userAnswer.questionAnswers[i].answerText.trim())
+              // const prompt = shortAnswer(question.questionText, question.correctAnswer, question.points, question.correctAnswer, userAnswer.questionAnswers[i].answerText.trim())
               
-              const result = await togetherManager.performInference(prompt);
-              console.log('result',result.trim());
-              userAnswer.questionAnswers[i].reason = result.replaceAll('\n','')
-              userAnswer.questionAnswers[i].point = 0;
-              // totalScore += 0; // Give full points if userAnswer.questionAnswers[i] is not empty
-              //   totalScore += question.points; // Give full points if userAnswer.questionAnswers[i] is not empty
+              // const result = await togetherManager.performInference(prompt);
+              // console.log('result',result.trim());
+              // userAnswer.questionAnswers[i].reason = result.replaceAll('\n','')
+              userAnswer.questionAnswers[i].point = question.points;
+              totalScore += 0; // Give full points if userAnswer.questionAnswers[i] is not empty
+                totalScore += question.points; // Give full points if userAnswer.questionAnswers[i] is not empty
             }
           }
         }
       } 
-      userAnswer.score = totalScore
-      await userAnswer.save()
-
-  // console.log(userAnswer.score)
-
-  // console.log(exam.privateAnswer)
-
-
-  // console.log(exam.privateScore)
+  userAnswer.score = totalScore
+  await userAnswer.save()
 
   var response = {}
-
-  // response.questions = questions  //questions
-  // response.userAnswer = userAnswer.questionAnswers // userAnswer.questionAnswers[answerText]  // score
-  // // response.exam = exam // exam.questions[correctAnswer] //
-  // response.score = totalScore 
 
   if(exam.privateScore){
     // totalScore = 0
@@ -148,20 +86,4 @@ exports.evaluateUserAnswer = catchAsync(async (req, res, next) => {
 
   res.status(200).send({response});
 
-  // const prompt =
-  //   "I am giving you an a question in a json file try to understand and and create an appropriate tags for this question and in your return make it a json file that has an object with a key tag and an array of string that are going to be the tags and make the tags short and concise and userAnswer.questionAnswers[i] me with a json format string in which i can take your output and use the json.parse" +
-  //   JSON.stringify({
-  //     question: question.questionText,
-  //     options: question.questionChoice,
-  //     type: question.questionType,
-  //   });
-
-  // const result = await togetherManager.performInference(prompt);
-
-  // console.log(result);
-  // const data = JSON.parse(result);
-
-  // // const data = parse(result)
-
-  // res.status(200).send({ message: result, data });
 });
