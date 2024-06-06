ff318fa7509c056d155bbaa02de5422213b3693d
Yohannes Mesganaw <yohannesmesg@gmail.com>
2024-06-06 22:29:36 +0300

    test issue

--- server/controller/auth/protect.js
+++ server/controller/auth/protect.js
@@ -1,8 +1,8 @@
 const { StatusCodes } = require("http-status-codes");
-const catchAsync = require("../../../utils/catchAsync");
-const { attachCookiesToResponse, isToken } = require("../../../utils/jwt");
-const APIError = require("../../../utils/apiError");
-const { TokenModel } = require("../../../models/Token.model");
+const catchAsync = require("../../utils/catchAsync");
+const { attachCookiesToResponse, isToken } = require("../../utils/jwt");
+const APIError = require("../../utils/apiError");
+const { TokenModel } = require("../../models/Token.model");
 
 exports.protect = catchAsync(async (req, res, next) => {
   const { refreshToken, accessToken } = req.signedCookies;
@@ -20,7 +20,6 @@ exports.protect = catchAsync(async (req, res, next) => {
       user: userId,
       refreshToken: payload.refreshToken,
     });
-
 
     if (!existingToken || !existingToken?.isValid) {
       return next(
