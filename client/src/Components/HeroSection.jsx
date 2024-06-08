dd8681e86e98d03ff1d91cc275b66acfaaec62e1
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2024-06-08 09:30:30 +0300

    fixed the home page

--- client/src/Components/HeroSection.jsx
+++ client/src/Components/HeroSection.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { AudioOutlined } from '@ant-design/icons';
 import { Input, Space } from 'antd';
+import { Icon } from '@iconify/react';
+import { Link } from 'react-router-dom';
 const { Search } = Input;
 const suffix = (
   <AudioOutlined
@@ -17,23 +19,29 @@ const HeroSection = () => {
           <div className="w-full max-w-[1248px] max-md:max-w-full">
             <div className="flex gap-5 max-md:flex-col max-md:gap-0">
               <div className="flex flex-col w-6/12 max-md:ml-0 max-md:w-full">
-                <div className="flex flex-col items-center self-stretch pt-1.5 pr-6 pb-8 my-auto max-md:mt-10 max-md:max-w-full">
+                <div className="flex flex-col items-center self-stretch pt-1.5 pr-6 pb-8 my-auto max-md:mt-16 max-md:max-w-full">
                   <h1 className="text-4xl font-bold leading-10 text-white max-md:mr-2.5 max-md:max-w-full">
                     Connecting Ethiopia's Top Talent <br /> with the Best Jobs.
                   </h1>
                   <p className="mt-8 text-base leading-7 text-white max-md:max-w-full">
-                    Empowering Ethiopians to find and create jobs, work and connect with <br /> professionals.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque neque natus, nemo quod blanditiis totam! Quis iste ea, consectetur quibusdam animi, corporis reiciendis voluptas suscipit quasi voluptatem optio! Earum, provident!
                   </p>
+<div className='flex flex-wrap gap-2 items-center justify-center mt-8'>
+<Link to='/sign-in'
+      type='button'
+      className="transition duration-300 ease-in-out flex items-center gap-2 bg-transparent hover:bg-primary-700 border text-white border-white font-semibold py-[0.4rem] px-16 rounded-md text-md">
+      Get Started 
+    </Link>
+    <a
+      href="#how-it-works"
+      type='button'
+      className="transition duration-300 ease-in-out flex items-center gap-2 bg-transparent hover:bg-primary-700 text-white  font-semibold py-[0.4rem] px-8 rounded-md text-md">
+      See How it works <Icon icon="mingcute:arrow-right-fill" />
+    </a>
+</div>
+                 
                
-    <Search
-      placeholder="Enter Exam key"
-      allowClear
-      enterButton="Search"
-      size="large"
-      className='w-96 mt-4'
-     
-    />
-                </div>
+          </div>
               </div>
               <div className="flex flex-col ml-5 w-6/12 max-md:ml-0 max-md:w-full">
                 <div className="flex flex-col grow justify-center px-16 max-md:px-5 max-md:mt-10 max-md:max-w-full">
