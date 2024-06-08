245f713ff62b39ac9fc5293e3ee6ba8cbc592f89
Tibebe Solomon <87706650+Tibex88@users.noreply.github.com>
2024-06-08 17:32:07 +0300

    minor url changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-// const apiUrl = import.meta.env.VITE_API_URL;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +8,7 @@ export default defineConfig({
     port: 4000,
     proxy: {
     '/api': {
-      target: process.env.VITE_API_URL,
+      target: process.env.VITE_TARGET_URL,
       secure: false,
       changeOrigin: true,
       ws: true,
