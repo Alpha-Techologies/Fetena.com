9e9f9a477eaa4e9e08e39c0d50f680e8382c1ed2
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2024-06-08 19:34:45 +0300

    did the about page and customer page

--- client/src/Screens/LandingPageScreens/About.jsx
+++ client/src/Screens/LandingPageScreens/About.jsx
@@ -1,24 +1,7 @@
-import React from 'react'
+import React from "react";
 import FeatureSection1 from "../../Components/FeatureSection1";
-import FeatureSection2 from "../../Components/FeatureSection2";
-import NavBar from "../../Components/NavBar";
-import Footer from "../../Components/Footer";
-import TestimonySection from "../../Components/TestimonySection";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
-
-const About = () => {
-
-  const navigate = useNavigate()
-
-  const { isAuthenticated } = useSelector((state) => state.auth)
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      navigate("/dashboard");
-    }
-  });
+import Faq from "../../Components/Faq";
+import { Card } from "antd";
 
 const featuresData = {
   title: "For Examiners or Examinees",
@@ -30,26 +13,42 @@ const featuresData = {
     "https://cdn.builder.io/api/v1/image/assets/TEMP/c896fd2f9a710e96a67f61747bd44eabe79c29f09d865cab24fd45c535cbefc8?apiKey=da0e5699a0964f23ab3a2091e7f935a3&",
 };
 
-
 const About = () => {
-  
   return (
-    <>
-      <h1 className='text-4xl font-bold'>About</h1>
-
-      <HeroSection />
-      <WhyChooseUsSection />
+    <div className="mt-28">
       <FeatureSection1 featuresData={featuresData} />
-      <FeatureSection2 featuresData={featuresData} />
-      <FeatureSection1 featuresData={featuresData} />
-      <FeatureSection2 featuresData={featuresData} />
 
-      <TestimonySection />
-      <HowItWorks />
+      <div className="mx-48 my-8">
+        <h1 className="text-3xl font-bold text-blue-900 mb-8 text-center">
+          FAQ's
+        </h1>
 
-      <Cta />
-      <Footer />
-    </>
+        <Faq />
+      </div>
+
+      <div className="mx-16 my-16">
+        <h1 className="text-3xl font-bold text-blue-900 mb-8 text-center">
+          Contact Us
+        </h1>
+
+        <div className=" flex flex-col justify-between px-32  gap-4">
+          <p className="font-semibold">
+            <span className="font-bold text-blue-700">Address : </span>Addis
+            Ababa Science & Technology University , Addis Ababa , Ethiopia
+          </p>
+          <div className="flex gap-2 items-center justify-center">
+            <p className="font-semibold">
+              <span className="font-bold text-blue-700">Email : </span>
+              fetena@gmail.com
+            </p>
+            <p className="font-semibold">
+              <span className="font-bold text-blue-700">Tel : </span>+2519456789
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
+
 export default About;
