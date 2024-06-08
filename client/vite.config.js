143f8f8df0f2b9f25400c9c2e12a3ffa3823e305
Yohannes Mesganaw <yohannesmesg@gmail.com>
2024-06-08 17:25:53 +0300

    working functioning straming

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-const apiUrl = import.meta.env.VITE_API_URL;
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+// const apiUrl = import.meta.env.VITE_API_URL;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:4000, 
+  server: {
+    port: 4000,
     proxy: {
-    '/api': {
-      target: apiUrl,
-      secure: false,
-      changeOrigin: true,
-      ws: true,
-      }
-  }}
-})
+      "/api": {
+        // target: process.env.VITE_API_URL.slice(0, -1),
+        target: "http://localhost:8080",
+        secure: false,
+        changeOrigin: true,
+        ws: true,
+      },
+    },
+  },
+});
