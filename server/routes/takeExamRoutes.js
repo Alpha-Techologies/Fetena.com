aa6bc71b35edf93e87e13a63ffd3021c62088323
Yohannes Teshome <83913756+johannesteshome@users.noreply.github.com>
2024-06-09 20:46:21 +0300

    fixed logger issue

--- server/routes/takeExamRoutes.js
+++ server/routes/takeExamRoutes.js
@@ -8,12 +8,13 @@ const {
 } = require("../controller/takeExam");
 
 const { protect } = require("../controller/auth");
+const { getOrganizationId } = require("../controller/organization/getOrganizationId");
 const router = express.Router();
 
 router.route("/start-exam/:id").post(protect, startExam);
 
 router.route("/exam-taker/:id").get(protect, getOneExamTaker);
-router.route("/take-exam/:id").patch(protect, updateTakeExam);
+router.route("/take-exam/:id").patch(protect, getOrganizationId, updateTakeExam);
 // get all the exam taken history of a exam in which is provided the examid in param
 router.route("/exam-history/:id").get(protect, getAllExamTaker);
 
