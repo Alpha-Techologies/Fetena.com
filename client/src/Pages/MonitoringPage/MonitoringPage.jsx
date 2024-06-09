89a549d523be678ae0b2e67f0f75912f2a9f7bbf
Yohannes Mesganaw <yohannesmesg@gmail.com>
2024-06-09 17:27:55 +0300

    commit the passage of the data from the parent component to the child component

--- client/src/Pages/MonitoringPage/MonitoringPage.jsx
+++ client/src/Pages/MonitoringPage/MonitoringPage.jsx
@@ -1,5 +1,5 @@
 import { Icon } from "@iconify/react";
-import { Select, Card,Tag  } from "antd";
+import { Select, Card, Tag } from "antd";
 import React, { useState, useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import useSocketIO from "../../utils/socket/useSocketIO";
@@ -15,6 +15,7 @@ import MonitoringTab from "./MonitoringTab";
 import ResultsTab from "./ResultsTab";
 import VideoMonitorWindow from "./VideoMonitorWindow";
 import { current } from "@reduxjs/toolkit";
+import CarouselComponent from "../../Components/CarouselComponent";
 
 const MonitoringPage = () => {
   const [activeTabKey1, setActiveTabKey1] = useState("tab1");
@@ -149,6 +150,10 @@ const MonitoringPage = () => {
     }
   }, [examStatus]);
 
+  useEffect(() => {
+    fetchExamineeList(currentExam._id);
+  }, [currentExam]);
+
   const tabList = [
     {
       key: "tab1",
@@ -215,16 +220,22 @@ const MonitoringPage = () => {
       {!examsList.length ? (
         <div>
           <div className="flex justify-between gap-4 items-center">
-          <h1 className='text-2xl font-bold text-blue-900 text-left mb-2'>Exam Monitoring</h1>
+            <h1 className="text-2xl font-bold text-blue-900 text-left mb-2">
+              Exam Monitoring
+            </h1>
           </div>
           <p>You currently have no exams created.</p>
         </div>
       ) : (
         <div>
           <div className="flex justify-between gap-4 items-center  mb-2">
-          <h1 className='text-2xl font-bold text-blue-900 text-left'>Exam Monitoring</h1>
+            <h1 className="text-2xl font-bold text-blue-900 text-left">
+              Exam Monitoring
+            </h1>
             <div className="flex items-center justify-center gap-4">
-              <span className="font-bold text-blue-900 text-[1rem]">Exam : </span>
+              <span className="font-bold text-blue-900 text-[1rem]">
+                Exam :{" "}
+              </span>
               <Select
                 defaultValue={examsList[0].value}
                 style={{
@@ -239,18 +250,21 @@ const MonitoringPage = () => {
             <Card>
               <div className="flex justify-between my-4">
                 <p className="text-lg">
-                  <span className="font-semibold">Exam :</span> <span className="font-bold text-blue-900"> {currentExam.examName}</span>
+                  <span className="font-semibold">Exam :</span>{" "}
+                  <span className="font-bold text-blue-900">
+                    {" "}
+                    {currentExam.examName}
+                  </span>
                 </p>
                 {examStatus === "open" ? (
                   <span className="text-success-500 font-semibold flex gap-2 items-center">
                     <Icon icon="heroicons-outline:status-online" />
                     <Tag color="green">Online</Tag>
-                  
-                    
                   </span>
                 ) : (
                   <span className="text-error-500 font-semibold flex gap-2 items-center">
-                    <Icon icon="codicon:eye-closed" />   <Tag color="red">Closed</Tag>{" "}
+                    <Icon icon="codicon:eye-closed" />{" "}
+                    <Tag color="red">Closed</Tag>{" "}
                   </span>
                 )}
               </div>
@@ -293,6 +307,14 @@ const MonitoringPage = () => {
                 </p>
               </div>
             </Card>
+            {seeStatusOf !== "all" &&
+              (currentUser.status !== "inprogress" ? (
+                <>
+                  <CarouselComponent />
+                </>
+              ) : (
+                <VideoMonitorWindow socket={socket} currentUser={currentUser} />
+              ))}
             <div className="flex gap-2 min-h-screen max-h-fit">
               <ExamineeListWindow
                 examineeList={examineeList}
@@ -315,38 +337,6 @@ const MonitoringPage = () => {
                   currentExam={currentExam}
                   socket={socket}
                 />
-                {seeStatusOf !== "all" &&
-                  (currentUser.status !== "inprogress" ? (
-                    <>
-                      {/* <Carousel fade>
-                        {currentUser?.userActivityLogs
-                          ?.filter((item) => !!item.imageUrl)
-                          ?.map((log, index) => {
-                            if (log.imageUrl) console.log(log, "log the image");
-                            return (
-                              <div key={index} className="carousel-slide">
-                                <img
-                                  src={`${import.meta.env.VITE_API_URL}${
-                                    log.imageUrl
-                                  }`}
-                                  alt="faceaiDetection"
-                                  className="w-128 h-128"
-                                />
-                              </div>
-                            );
-                          })}
-                      </Carousel> */}
-                      <img
-                        src="http://localhost:8080/uploads/faceaiDetections/faceaiDetection_66644bb81dcbc090c199e4b7_1717899895553.png"
-                        alt="faceapi"
-                      />
-                    </>
-                  ) : (
-                    <VideoMonitorWindow
-                      socket={socket}
-                      currentUser={currentUser}
-                    />
-                  ))}
               </div>
             </div>
           </div>
