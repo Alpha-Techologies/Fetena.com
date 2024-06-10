a5312a83689ec8771294194028e2ba9dfdb17a57
Yohannes Mesganaw <yohannesmesg@gmail.com>
2024-06-10 08:33:31 +0300

    exam screen updates

--- server/routes/userAnswerRoutes.js
+++ server/routes/userAnswerRoutes.js
@@ -1,31 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
-
 const {
-    createUserAnswer,
-    deleteUserAnswer,
-    getAllUserAnswer,
-    getOneUserAnswer,
-    updateUserAnswer,
-    evaluateUserAnswer
-} = require('../controller/userAnswer')
+  createUserAnswer,
+  deleteUserAnswer,
+  getAllUserAnswer,
+  getOneUserAnswer,
+  updateUserAnswer,
+  evaluateUserAnswer,
+} = require("../controller/userAnswer");
+const { protect } = require("../controller/auth");
 
 router
-    .route("/:id")
-    .get(getOneUserAnswer)
-    .post(createUserAnswer)
+  .route("/:id")
+  .get(protect, getOneUserAnswer)
+  .post(protect, createUserAnswer);
 
-router
-    .route("/eval/:id")
-    .post(evaluateUserAnswer)
-    // .post(evaluateUserAnswer)
-    // .put(updateQuestion)
-    // .delete(deleteQuestion);
+router.route("/eval/:id").post(protect, evaluateUserAnswer);
+// .post(evaluateUserAnswer)
+// .put(updateQuestion)
+// .delete(deleteQuestion);
 
-router
-    .route("/")
-        // .get(getAllQuestion)
-        
+// router.route("/")
+// .get(getAllQuestion)
 
 module.exports = router;
